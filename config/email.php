4ad3f9c1831f22a1031e28adee64de2706fe616f
WilaWara <wilawara@outlook.com>
2021-02-20 18:15:10 -0400

    Actualizacion de email oficial

--- config/email.php
+++ config/email.php
@@ -2,9 +2,9 @@
     $direccion_formulario = $_POST["direccion_formulario"];
     $mensaje_formulario = $_POST["mensaje_formulario"];
 
-	$subject = "Mensaje de miembro nuevo";
-	$to = "wilawara@outlook.com";
-	$headers = "From: Rey_Soberano" . "\r\n";
+	$subject = "Mensaje desde BioBolivia.tech";
+	$to = "jaimerodriguez0001@gmail.com";
+	$headers = "From: Sitio_BioBolivia" . "\r\n";
 	$headers .= "Content-type: text/html\r\n";
 
 	$body = "Email: " . $direccion_formulario . "<br>Mensaje: " . $mensaje_formulario;
